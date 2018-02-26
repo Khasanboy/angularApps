ee5ad4160f25149c42d2b07064ec39a113f745f9
Khasanboy <akbarov8808@gmail.com>
2018-02-26 23:31:32 +0200

    authentication started

--- fruitStore/src/app/components/navbar/navbar.component.ts
+++ fruitStore/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
+import { AngularFireAuth } from 'angularfire2/auth';
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+  user$: Observable<firebase.User>;
+  constructor(private afAuth:AngularFireAuth) {
+    this.user$ = afAuth.authState;
+   }
 
   ngOnInit() {
+  }
+
+  logout(){
+    this.afAuth.auth.signOut();
   }
 
 }
